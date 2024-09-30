e4e85f14c1f02f1a44c5e53130870a328ae2b43f
arpitaga04 <arpitaga03@gmail.com>
2024-09-30 11:50:26 +0530

    Add lspconfig for terraformls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -30,7 +30,6 @@ local servers = {
   "jsonls",
   -- terraform
   "terraformls",
-  "terraform_lsp",
 }
 
 local nvlsp = require "nvchad.configs.lspconfig"
@@ -44,6 +43,18 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.terraformls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    filetypes = {
+      "teraform",
+      "terraform-vars",
+      "tf",
+    },
+  },
+}
 
 lspconfig.yamlls.setup {
   on_attach = nvlsp.on_attach,
@@ -58,7 +69,7 @@ lspconfig.yamlls.setup {
         -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
         url = "",
       },
-      schemas = require('schemastore').yaml.schemas(),
+      schemas = require("schemastore").yaml.schemas(),
       -- schemas = {
       --   ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
       --   ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
@@ -67,7 +78,7 @@ lspconfig.yamlls.setup {
       --   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yaml",
       -- },
     },
-  }
+  },
 }
 lspconfig.jsonls.setup {
   on_attach = nvlsp.on_attach,
@@ -75,7 +86,7 @@ lspconfig.jsonls.setup {
   capabilities = nvlsp.capabilities,
   settings = {
     json = {
-      schemas = require('schemastore').json.schemas(),
+      schemas = require("schemastore").json.schemas(),
       validate = { enable = true },
     },
   },
